ad576ac53f99274dd384f1b41359098f69af60b4
NarimanBaba <narimanfb@code.edu.az>
2021-02-14 09:45:49 -0800

    nnn

---
+++ myhomeTasks/myhomeTasks/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace myhomeTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 1 tapsiriq
+            //int num = Int32.Parse(Console.ReadLine());
+            //if (num % 2 == 0)
+            //{
+            //    Console.WriteLine("cut ededdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("tek ededdir");
+            //}
+            //Rectangle Area: A (area) = L (length) x W (width)
+
+
+            //2 tapsiriq
+            //Console.Write("uzunluqu daxil ediin: ");
+            //int lengthSide = Int32.Parse(Console.ReadLine());
+            //Console.Write("eni d: ");
+            //int widthSide = Int32.Parse(Console.ReadLine());
+
+            //int area = lengthSide * widthSide;
+
+            //Console.WriteLine("sahesi: {0}", area);
+            //int perimetr = 2 * (lengthSide + widthSide);
+            //Console.WriteLine("perimetri: {0}", perimetr);
+
+            //3 tapsiriq
+            //Console.WriteLine("ededi daxil edin");
+            //int num = Int32.Parse(Console.ReadLine());
+
+            //Console.WriteLine(  num*num);
+
+            //4 tapsiriq
+            //int num = Int32.Parse(Console.ReadLine());
+            //if (num>0)
+            //{
+            //    Console.WriteLine("eded musbetdir");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded menfidir");
+            //}
+
+            //5 tapsiriq
+            //int num = Int32.Parse(Console.ReadLine());
+            //int reverse = 0;
+            //int remainder;
+            //while (num>0)
+            //{
+            //    remainder = num % 10;
+            //    reverse = (reverse * 10) + remainder;
+            //    num = num / 10;
+
+
+
+            //}
+            //Console.WriteLine(reverse);
+
+            //6 tapsiriq
+
+            //int sum = 0;
+
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    sum+=i;
+
+            //}
+            //Console.WriteLine(sum);
+
+
+
+            //7 tapsiriq
+
+
+            //int num = Int32.Parse(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+        }
+        
+
+   
+            } }
